1216c8e8d050a8eaca9fb4014984857f5de56615
DanielKahma <>
2023-10-10 12:05:54 +0000

    set up struction for game

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function canculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaysubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
