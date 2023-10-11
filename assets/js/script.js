6aa270e2383b554241aeef0ebb3caca6abe780a4
DanielKahma <>
2023-10-11 07:55:45 +0000

    add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,10 +45,11 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Nice! You got it right! ;D");
+        incrementScore();
 
     } else {
         alert(`Aaawwww... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-    }
+    } incrementWrongAnswer();
 
     runGame(calculatedAnswer[1]);
 }
@@ -72,25 +73,38 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1.
+ */
+
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1.
+ */
 
 function incrementWrongAnswer() {
+    function incrementScore() {
+        let oldScore = parseInt(document.getElementById("incorrect").innerText);
+        document.getElementById("incorrect").innerText = ++oldScore;
 
-}
+    }
 
-function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
+    function displayAdditionQuestion(operand1, operand2) {
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
 
-}
+    }
 
-function displaySubtractQuestion() {
+    function displaySubtractQuestion() {
 
-}
+    }
 
-function displayMultiplyQuestion() {
+    function displayMultiplyQuestion() {
 
-}
+    }
